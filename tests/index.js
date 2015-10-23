d516534d0c20d5afa1888370519f9af7caf11689
Craig Bilner <craig.bilner@gmail.com>
2015-10-23 20:51:16 +0100

    Add initial test rig to build testing examples on

---
+++ tests/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Postcard from './components/postcard/postcard';
+
+ReactDOM.render(<Postcard />, document.getElementById('root'));
+
---
+++ tests/webpack.config.test.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    './tests/index'
+  ],
+  output: {
+    path: path.join(__dirname),
+    filename: 'index.min.js'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname)
+    }]
+  }
+};
