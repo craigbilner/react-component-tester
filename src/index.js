81d1d49cd98a73bbacc61f2553ded7761f798258
Craig Bilner <craig.bilner@gmail.com>
2015-10-24 14:59:56 +0100

    Add findChild method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,81 @@
 import stampit from 'stampit';
+import _ from 'lodash';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 
+// FLAVOUR
+
+// METHODS
+
+const flavourInit = function ({instance}) {
+  this.childMap =
+    this.convertAndReduce(instance.children);
+};
+
+const convertAndReduce = function (children, parentIndx) {
+  return React
+    .Children
+    .toArray(children)
+    .reduce(
+    this.reduceChildren.bind(this, parentIndx),
+    {}
+  );
+};
+
+const reduceChildren =
+    function (parentIndx, childMap, child, indx) {
+      const childIsElement = TestUtils.isElement(child);
+
+      if (childIsElement) {
+        const id = parentIndx ? parentIndx + '.' + indx : indx;
+        childMap[id] = {
+          type: child.type,
+          style: child.props.style
+        };
+
+        if (TestUtils.isElement(child.props.children)) {
+          _.assign(
+            childMap,
+            this.convertAndReduce(
+              child.props.children,
+              id
+            )
+          );
+        } else if (child.props.children) {
+          const soloId = id + '.0';
+          childMap[soloId] = {
+            value: child.props.children
+          };
+        }
+      }
+      else {
+        childMap[indx] = {
+          value: child
+        };
+      }
+
+      return childMap;
+    }
+  ;
+
+const findChild = function (path) {
+  return this.childMap[path];
+};
+
+const flavourMethods = {
+  convertAndReduce,
+  reduceChildren,
+  findChild
+};
+
+const flavour = stampit()
+  .init(flavourInit)
+  .methods(flavourMethods);
+
+// TESTER
+
 // INIT
-const init = function () {
+const testerInit = function () {
   this.component = null;
   this.flavours = new Map();
 };
@@ -14,38 +86,39 @@ const use = function (component) {
   return this;
 };
 
-const addFlavour = function (name, flavour) {
+const createFlavour = function ({type, props}) {
+  return flavour({
+    type,
+    style: props.style,
+    children: props.children
+  });
+};
+
+const addFlavour = function (name, props) {
   const shallowRenderer = TestUtils.createRenderer();
 
   shallowRenderer
     .render(React.createElement(
       this.component,
-      flavour
+      props
     ));
-
-  const {
-    type,
-    props
-    } = shallowRenderer.getRenderOutput();
 
   this.flavours.set(
     name,
-    {
-      type,
-      style: props.style
-    }
+    this.createFlavour(shallowRenderer.getRenderOutput())
   );
 };
 
-const methods = {
+const testerMethods = {
   use,
+  createFlavour,
   addFlavour
 };
 
 // STAMP
 const ReactTester =
   stampit()
-    .init(init)
-    .methods(methods);
+    .init(testerInit)
+    .methods(testerMethods);
 
 export default ReactTester.create();
--- tests/components/app/app-tests.js
+++ tests/components/app/app-tests.js
@@ -9,8 +9,8 @@ const flavours = {
   NO_SIGNATURE: 3
 };
 tester.addFlavour(flavours.NONE, {});
-tester.addFlavour(flavours.WITH_SIGNATURE, {});
-tester.addFlavour(flavours.NO_SIGNATURE, {});
+// tester.addFlavour(flavours.WITH_SIGNATURE, {});
+// tester.addFlavour(flavours.NO_SIGNATURE, {});
 
 describe('app should', () => {
   it('render as a div', () => {
@@ -20,6 +20,37 @@ describe('app should', () => {
         .get(flavours.NONE)
         .type;
     const expected = 'div';
+    assert.deepEqual(actual, expected);
+  });
+
+  it('display as a flex row', () => {
+    const actual =
+      tester
+        .flavours
+        .get(flavours.NONE)
+        .style;
+    const expected = {
+      display: 'flex',
+      flexFlow: 'row'
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('have a right gutter', () => {
+    const rightGutter =
+      tester
+        .flavours
+        .get(flavours.NONE)
+        .findChild('2');
+    const actual = rightGutter.style;
+    const expected = {
+      flex: 0,
+      '@media (min-width: 400px)': {
+        flex: 1
+      }
+    };
+
     assert.deepEqual(actual, expected);
   });
 });
