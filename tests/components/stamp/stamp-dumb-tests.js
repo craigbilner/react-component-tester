e0a15bce2cae95aa707dfedb24b424b24c8605be
Craig Bilner <craig.bilner@thetrainline.com>
2015-11-05 11:14:31 +0000

    Lint files

--- tests/components/stamp/stamp-dumb-tests.js
+++ tests/components/stamp/stamp-dumb-tests.js
@@ -5,15 +5,15 @@ import ReactTester from '../../../src/index';
 
 const tester = ReactTester.create().use(Stamp);
 const NONE = tester.addFlavour('NONE', {
-    types,
-    type: types.NONE
+  types,
+  type: types.NONE,
 });
 
 describe('dumb stamp should', () => {
-    it('render the place stamp here text if the type is none', () => {
-        const actual = NONE.findChild('0').value;
-        const expected = 'Place stamp here';
+  it('render the place stamp here text if the type is none', () => {
+    const actual = NONE.findChild('0').value;
+    const expected = 'Place stamp here';
 
-        assert.deepEqual(actual, expected);
-    });
+    assert.deepEqual(actual, expected);
+  });
 });
