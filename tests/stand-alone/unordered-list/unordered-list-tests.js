598c11b50c9f9322747c516f974e760ab1dec32e
Craig Bilner <craig.bilner@thetrainline.com>
2015-11-05 11:26:33 +0000

    Add first stand alone test

---
+++ tests/stand-alone/unordered-list/unordered-list-tests.js
@@ -0,0 +1,22 @@
+import assert from 'assert';
+import UnorderedList from './unordered-list';
+import ReactTester from '../../../src/index';
+
+const tester = ReactTester.create().use(UnorderedList);
+const i18n = {
+  help: 'test help',
+  something: 'test something',
+};
+const NO_LINKS = tester.addFlavour('NO_LINKS', {
+  i18n,
+  links: [],
+});
+
+describe('unordered list should', () => {
+  it('render the first anchor with the expected title with no dynamic links', () => {
+    const actual = NO_LINKS.findChild('0.0').props.title;
+    const expected = 'test help';
+
+    assert.deepEqual(actual, expected);
+  });
+});
---
+++ tests/stand-alone/unordered-list/unordered-list.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default ({ i18n, links }) => (
+    <ul className="class-1">
+        <li className="class-2">
+            <a href="some-href-1"
+               title={i18n.help}
+               target="_blank"
+               rel="help">
+                {i18n.help}
+            </a>
+        </li>
+        <li className="class-3">
+            <a href="some-href-2">
+                {i18n.something}
+            </a>
+        </li>
+        {
+            links.map(link => <li key={link.title}>{link}</li>)
+        }
+    </ul>
+);
